600c012a216ab20af933aad1c715894cdeaf52f1
SubediSb <babu.pkr2@gmail.com>
2023-02-09 11:27:43 -0600

    added

---
+++ additional.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class additional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		additionA(1);
+		additionA(11,22);
+		additionA(11,22,33);
+		
+	}
+	
+	// overloading 
+	// same class , same method name , differnet signature
+	
+	
+	static void additionA(int x){
+		System.out.println(x);
+	}
+	
+	static void additionA(int x ,int y){
+		System.out.println(x+y);
+	}
+	
+	static void additionA(int x ,int y ,int z){
+		System.out.println(x+y+z);
+	}
+
+}
